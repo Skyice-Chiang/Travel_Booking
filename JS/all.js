0b903b7c3505d41e4a00d9678948e1d630c27b3e
Skyice-Chiang <vicky12218@gmail.com>
2020-10-09 19:37:49 +0800

    update reserve system
    改善進入預訂系統的畫面

--- JS/all.js
+++ JS/all.js
@@ -154,12 +154,14 @@ function lightboxOption() {
 //reserve rooms
 function reserve(){
     window.scrollTo(0,356);
+    document.querySelector("body").classList.add("active");
     document.querySelector(".booking_view").classList.add("active");
     document.querySelector(".booking_info").classList.add("active");
 }
 
 //cancel reserve
 function cancel(){
+    document.querySelector("body").classList.remove("active");
     document.querySelector(".booking_view").classList.remove("active");
     document.querySelector(".booking_info").classList.remove("active");
 }
